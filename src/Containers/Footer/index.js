8e67f0cf34412cb71e9140be1d511e696f52f8ee
Alexandre dos Santos Soares <ale_santos.soares@hotmail.com>
2018-12-11 14:55:19 -0200

    containers: created footer component

---
+++ src/Containers/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.css'
+
+const Footer = () => (
+  <div className="l-footer">
+    <button className="cancel-button primary">cancelar</button>
+    <button className="agree-button">confirmar</button>
+  </div>
+)
+
+export default Footer
