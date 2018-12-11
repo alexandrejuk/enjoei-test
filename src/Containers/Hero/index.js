4bf8a214da791b76a12c40cbb982ec88e6f6ece8
Alexandre dos Santos Soares <ale_santos.soares@hotmail.com>
2018-12-11 14:54:29 -0200

    containers: created hero component

---
+++ src/Containers/Hero/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+const Hero = () => (
+  <div className="l-hero">
+    <img 
+      className="l-product" 
+      src="https://ae01.alicdn.com/kf/HTB1aN.kKFXXXXarXXXXq6xXFXXX5/Entrega-r-pida-Novo-2015-roupas-da-menina-das-crian-as-vestido-de-crian-a-e.jpg" 
+      alt="product-item"
+      />
+  </div>
+)
+
+export default Hero
