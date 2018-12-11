c384e61ad8dfbe32b21a78c6d5fd12307fad9ecb
Alexandre dos Santos Soares <ale_santos.soares@hotmail.com>
2018-12-11 14:54:03 -0200

    containers: created header component

---
+++ src/Containers/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+const Header = () => (
+  <div className="l-header">
+    <div className="logo">
+      <img src="https://pbs.twimg.com/profile_images/989824346607714305/iykqbmjw_400x400.jpg" alt="logo" />
+    </div>
+  </div>
+)
+
+export default Header
