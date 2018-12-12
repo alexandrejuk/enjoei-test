b66426dcac295f37873a7d2f65c7a8baaed53941
Alexandre dos Santos Soares <ale_santos.soares@hotmail.com>
2018-12-12 10:32:21 -0200

    component: add event for button

--- client/src/Components/Button/index.js
+++ client/src/Components/Button/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './index.css'
 
-const Button = ({ name, classStyle }) => (
-  <button className={`button ${ classStyle }`}>{ name }</button>
+const Button = ({ name, classStyle, handleClick, type }) => (
+  <button onClick={()=> handleClick(type)} className={`button ${ classStyle }`}>{ name }</button>
 )
 
 export default Button
